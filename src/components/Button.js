07213afc70cb010d83a068b670dd6e48592c80dd
BrainClouder <celso.w.dias.a@gmail.com>
2020-05-14 03:28:25 -0300

    creating button component with stories and unity test, it can render several colors and have an animated loading icon

---
+++ src/components/Button.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { useMemo } from 'react';
+
+const rotate = keyframes`
+    0% {
+        transform: rotate(0deg);
+    }
+    50% {
+        transform: rotate(180deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+`;
+
+const LoadingIcon = styled.div`
+    width: 18px;
+    height: 18px;    
+    border-radius: 50%;    
+    &.animated {
+        animation: ${rotate} 5s linear infinite;
+    }
+        border: 2px solid #000000bb;
+        border-left: 2px solid #00000055;
+        box-shadow: 2px 0px 4px #00000055, inset 0 0 10px #aaaaaa56;
+
+    &.primary {
+        border: 2px solid #ffffffdd;
+        border-left: 2px solid #aaaaaa55;
+        box-shadow: 2px 0px 4px #ffffff55, inset 0 0 10px #00000056;
+    }
+`;
+
+const ButtonContainer = styled.div`
+    font-family: Helvetica, sans-serif, apple-system;
+    display: inline-block;
+    font-size: 18px;    
+    &.btn-primary {
+        background-color: hsl(265, 80%, 45%);
+        box-shadow: 2px 2px 4px #00000075;
+        color: white;
+        border: 1px solid #00000025;
+        &.btn-color-red {
+            background-color: hsl(350, 70%, 45%);
+        }
+        &.btn-color-green {
+            background-color: hsl(150, 70%, 45%);
+        }
+        &.btn-color-orange {
+            background-color: hsl(25, 70%, 55%);
+        }
+    }
+
+    &.btn-secondary {
+        background-color: transparent;
+        color: hsl(265, 80%, 45%);
+        border: 1px solid hsl(265, 80%, 45%);
+        &.btn-color-red {
+            color: hsl(350, 70%, 45%);
+            border-color: hsl(350, 70%, 45%);
+        }
+        &.btn-color-green {
+            color: hsl(150, 70%, 45%);
+            border-color: hsl(150, 70%, 45%);
+        }
+        &.btn-color-orange {
+            color: hsl(25, 70%, 55%);
+            border-color: hsl(25, 70%, 55%);
+        }
+    }
+
+    &.btn-text {
+        background-color: transparent;
+        border: transparent;
+        color: hsl(265, 40%, 45%);
+        &.btn-color-red {
+            color: hsl(350, 70%, 45%);
+        }
+        &.btn-color-green {
+            color: hsl(150, 70%, 45%);
+        }
+        &.btn-color-orange {
+            color: hsl(25, 70%, 55%);
+        }
+    }
+
+    &.btn-small {
+        padding: 0.12em 0.25em;
+    }
+    &.btn-medium {
+        padding: 0.25em 0.5em;
+    }
+    &.btn-large {
+        padding: 0.5em 1em;
+    }
+
+    &.btn-rounded-full {
+        border-radius: 100px;
+    }
+
+    &.btn-rounded-lg {
+        border-radius: 5px;
+    }
+
+    &.btn-on {
+        cursor: pointer;
+        transition: 500ms;
+        &:hover {
+            background-image: radial-gradient(#00000005, #00000028);
+        }
+    }
+    &.btn-off {
+        opacity: 0.5;
+    }
+`;
+
+const Button = (props) => {
+    useMemo(() => [props.children, props.type]);
+    return <ButtonContainer 
+    className={`btn-${props.type ? props.type : 'primary'} 
+    btn-${props.small ? 'small' : props.medium ? 'medium' : 'large'}
+    btn-${props.rounded ? 'rounded-full' : 'rounded-lg'}
+    btn-color-${props.color}
+    btn-${props.disabled ? 'off' : 'on'}`}    
+    >
+        {props.loading && <LoadingIcon className={`${props.animated && 'animated'} ${props.type}`} />}
+        {props.children}
+    </ButtonContainer>
+}
+
+export default Button;
--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import {storiesOf} from '@storybook/react';
+import Button from '../components/Button';
 
-export default {
-  title: 'Button',
-  component: Button,
-};
-
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
-
-export const Emoji = () => (
-  <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
-);
+storiesOf('Button', module)
+  .add('main', () => <Button type="primary">Button</Button>)
+  .add('secondary', () => <Button type="secondary">Button</Button>)
+  .add('text', () => <Button type="text">Button</Button>)
+  .add('small primary', () => <Button type="primary" small>Button</Button>)
+  .add('medium primary', () => <Button type="primary" medium>Button</Button>)
+  .add('large primary', () => <Button type="primary" large>Button</Button>)
+  .add('rounded primary', () => <Button type="primary" rounded>Button</Button>)
+  .add('red primary', () => <Button type="primary" color="red">Button</Button>)
+  .add('red secondary', () => <Button type="secondary" color="red">Button</Button>)
+  .add('red text', () => <Button type="text" color="red">Button</Button>)
+  .add('orange primary', () => <Button type="primary" color="orange">Button</Button>)
+  .add('orange secondary', () => <Button type="secondary" color="orange">Button</Button>)
+  .add('orange text', () => <Button type="text" color="orange">Button</Button>)
+  .add('green primary', () => <Button type="primary" color="green">Button</Button>)
+  .add('green secondary', () => <Button type="secondary" color="green">Button</Button>)
+  .add('green text', () => <Button type="text" color="green">Button</Button>)
+  .add('primary loading animated', () => <Button loading animated type="primary"></Button>)
+  .add('secondary loading animated', () => <Button loading animated type="secondary"></Button>)
+  .add('text loading animated', () => <Button loading animated type="text"></Button>)
