387248c8a83983e4887866eb59e3569c73a07ba3
BrainClouder <celso.w.dias.a@gmail.com>
2020-05-14 01:57:58 -0300

    to the cloud! fist commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('Renders without crashing', () => {
+    const wrapped = shallow(<App />);
+    expect(wrapped.find('code').length).toEqual(1);
+  });
+})
